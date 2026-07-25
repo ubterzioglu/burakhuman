1d14985aae1294aaf03f4e40a8e8c809906c3ad8
Umut Baris Terzioglu <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-07-25 22:01:28 +0200

    Align footer backlinks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,13 +16,7 @@ export function Footer() {
             Firması Ufuksoy Nakliyat A.Ş
           </p>
           <p className="partner-links">
-            <span><a href="https://tekhurdametal.com/istanbul-hurdaci/" rel="dofollow">İstanbul Hurdacı</a> Firması Tek Hurda Metal A.Ş</span>{" · "}
-            <span><a href="https://tekhurdametal.com/hurda-fiyatlari/" rel="dofollow">Hurda Fiyatları</a> Tek Hurda Metal</span>{" · "}
-            <span><a href="https://tekhurdametal.com/hurda-demir-fiyatlari/" rel="dofollow">Demir Hurda Fiyatları</a> Tek Hurda Metal</span>{" · "}
-            <span><a href="https://tekhurdametal.com/beylikduzu-hurdaci/" rel="dofollow">Beylikdüzü Hurdacı</a> Firması Tek Hurda Metal A.Ş</span>{" · "}
-            <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">Antalya Sex Shop</a> Lion Erotik</span>{" · "}
-            <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">Antalya Erotik Shop</a> Lion Erotik</span>{" · "}
-            <span><a href="https://lionerotik.com/urunler/fetis-urunleri" rel="dofollow">Antalya Seks Shop</a> Lion Erotik</span>
+            <span><a href="https://tekhurdametal.com/hurda-demir-fiyatlari/" rel="dofollow">Demir Hurda Fiyatları</a> Tek Hurda Metal</span>
           </p>
           <p>
             Coding by <a href="http://www.yazarge.com">YAZARGE</a>
